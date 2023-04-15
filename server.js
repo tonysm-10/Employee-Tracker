7eb54275d64f00eb695995d34757a219afbdecf8
tonysm-10 <ayalaarellanoanthony@gmail.com>
2023-04-14 17:58:07 -0700

    I made the if statement for when the user selects to add a department, it will call the function addDepartment and in that function it will prompt the user to enter a department and that input is then stored in a variable which is then inserted into the table using connection.query

--- server.js
+++ server.js
@@ -13,35 +13,46 @@ connection.connect((err) => {
   if (err) throw err;
   console.log('Connected!');
 });
-function userInput(){
+function userInput() {
     inquirer.prompt([
-        {
-          type: 'list',
-          name: 'start',
-          message: 'What do you want to do?',
-          choices: ['View all Departments', 'View all Roles', 'View all Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role']
-        },
-      ]).then(function (answers) {
-        if (answers.start === 'View all Departments') {
-          getAllDepartments(); 
-          console.log('You chose to get all Departments');
-          console.log()
-        }
-        if (answers.start === 'View all Roles') {
-            getAllRoles()
-            console.log('You chose to get all Roles')
-            console.log()
-        }
-        if (answers.start === 'View all Employees') {
-            getAllEmployees()
-            console.log('You chose to get all Employees')
-            console.log()
-        }
-      });
-}
-
+      {
+        type: 'list',
+        name: 'start',
+        message: 'What do you want to do?',
+        choices: ['View all Departments', 'View all Roles', 'View all Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role']
+      },
+    ]).then(function (answers) {
+      // User selection if Statements to call the appropriate function to log data from database
+      if (answers.start === 'View all Departments') {
+        getAllDepartments();
+        console.log('You chose to get all Departments');
+        console.log()
+      }
+      else if (answers.start === 'View all Roles') {
+        getAllRoles()
+        console.log('You chose to get all Roles')
+        console.log()
+      }
+      else if (answers.start === 'View all Employees') {
+        getAllEmployees()
+        console.log('You chose to get all Employees')
+        console.log()
+      }
+      else if (answers.start === 'Add a Department') {
+        addDepartment()
+        console.log()
+      }
+      else {
+        console.log('Invalid choice. Please choose a valid option.');
+        userInput(); // Prompt user again for input
+      }
+    });
+  }
   
 
+//////////// All the functions to retrieve or add data to the database ////////////
+
+// Function to log the Departments table from db
 function getAllDepartments() {
   // Perform MySQL query to retrieve departments data
   connection.query('SELECT * FROM department', function (err, results) {
@@ -51,6 +62,8 @@ function getAllDepartments() {
     userInput()
   });
 }
+
+// Function to log the Roles table from db
 function getAllRoles(){
     connection.query('SELECT * FROM roles_view;', function (err, results) {
         if (err) throw err;
@@ -59,7 +72,7 @@ function getAllRoles(){
         userInput()
       }); 
 }
-
+// Function to log the Employees table from db
 function getAllEmployees(){
     connection.query('SELECT * FROM employee_view;', function (err, results) {
         if (err) throw err;
@@ -67,6 +80,26 @@ function getAllEmployees(){
         console.table(results);
         userInput()
       }); 
+}
+// Add data to db 
+function addDepartment(){
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "addDepartment",
+            message: "Enter the name of the department!"
+        }
+    ]).then(function(input){
+        const addDepo = input.addDepartment;
+        console.log('You have added ' + addDepo);
+        console.log()
+        // Perform MySQL query to insert department data
+        connection.query('INSERT INTO department (name) VALUES (?)', [addDepo], function (err, results) {
+            if (err) throw err;
+            // Log departments data using console.table
+            userInput();
+        });
+    });
 }
 
 userInput()
